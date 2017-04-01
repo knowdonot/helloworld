fff6d893f5660525d0430372f52fac0d453fc47e
knowdonot <knowdonot@Josiah-PC>
2017-04-01 15:29:15 +0800

    init

--- src/com/duoduo/junior/HelloWorld.java
+++ src/com/duoduo/junior/HelloWorld.java
@@ -1,6 +1,12 @@
 package com.duoduo.junior;
 
 public class HelloWorld {
+	/**
+	 * 程序入口
+	 * 2017年4月1日
+	 * By duoduo
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
